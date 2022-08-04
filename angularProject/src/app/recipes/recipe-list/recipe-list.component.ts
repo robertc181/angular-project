b33ec7a6f55e5370cb0006611eed47ff7fd5d3f3
robertc181 <rdancole@gmail.com>
2022-08-04 19:06:47 +0000

    add structure of recipe

--- angularProject/src/app/recipes/recipe-list/recipe-list.component.ts
+++ angularProject/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+
+  recipes: Recipe[] = [
+    new Recipe ('A test Recipe', 'This is simply a test', 'https://www.delicious.com.au/recipes/collections/gallery/60-indian-recipes-to-warm-you-up-this-winter/i66gak0q'),
+    new Recipe ('A test Recipe', 'This is simply a test', 'https://www.delicious.com.au/recipes/collections/gallery/60-indian-recipes-to-warm-you-up-this-winter/i66gak0q'),
+  ];
 
   constructor() { }
 
